88ef4d610f9be043062a476ab54548718ac1c39e
KoDongUk <bogo1357@hanmail.net>
2022-10-19 18:10:06 +0900

    feature: vue-router 셋 및 기본 path 설정

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/router";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  router,
+  el: "#app",
   render: h => h(App)
-})
+});
---
+++ src/views/MainView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header/Header.vue";
+import Footer from "../components/Footer/Footer.vue";
+
+export default {
+  name: "MainView",
+  components: {
+    Header: Header,
+    Footer: Footer
+  }
+};
+</script>
+
+<style></style>
