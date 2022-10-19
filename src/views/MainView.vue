3db79401643a47bfd64f0f5723e4b861c1955952
KoDongUk <bogo1357@hanmail.net>
2022-10-19 18:33:06 +0900

    feature: toDos Component 호출

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,18 +2,24 @@
   <div>
     <Header />
     <Footer />
+    <ToDoInput />
+    <ToDoList />
   </div>
 </template>
 
 <script>
 import Header from "../components/Header/Header.vue";
 import Footer from "../components/Footer/Footer.vue";
+import ToDoInput from "../components/ToDos/ToDoInput.vue";
+import ToDoList from "../components/ToDos/ToDoList.vue";
 
 export default {
   name: "MainView",
   components: {
     Header: Header,
-    Footer: Footer
+    Footer: Footer,
+    ToDoInput: ToDoInput,
+    ToDoList: ToDoList
   }
 };
 </script>
